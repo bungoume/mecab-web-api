2c9c392f11c2559198f63ccaa26cc86b6cc67a79
umezaki <bungoume3@yahoo.co.jp>
2015-03-27 02:01:44 +0900

    allow post request

--- text_analysis/main/views.py
+++ text_analysis/main/views.py
@@ -1,11 +1,13 @@
 from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
 
 from main import mecab_utils
 from main.forms import ReadingForm, ParseForm
 
 
+@require_http_methods(["GET", "POST"])
 def reading(request):
-    form = ReadingForm(request.GET)
+    form = ReadingForm(request.REQUEST)
     if not form.is_valid():
         return JsonResponse(
             {"error": {"code": "form_invalid", "errors": form.errors}}, status=400)
@@ -20,8 +22,9 @@ def reading(request):
     return JsonResponse(ret)
 
 
+@require_http_methods(["GET", "POST"])
 def parse(request):
-    form = ParseForm(request.GET)
+    form = ParseForm(request.REQUEST)
     if not form.is_valid():
         return JsonResponse(
             {"error": {"code": "form_invalid", "errors": form.errors}}, status=400)
